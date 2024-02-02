035e0e949b5f13f50cc2b87b0977fe7c3a14c427
smt7174 <suzukima@belltreesoft.jp>
2024-02-03 07:49:22 +0900

    Initial commit

---
+++ test/private-func-mock.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PrivateFuncMock from '../lib/private-func-mock-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/private-func-mock-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PrivateFuncMock.PrivateFuncMockStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
